ce75b8c8d1b8a98ffb54e121f5998de504f71ca3
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-04-15 15:04:15 +0600

    updated firebase config

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9d0uI_Q2dUZatNwUIcwpvta71w1hZGjo",
-  authDomain: "assignment9-realestates.firebaseapp.com",
-  projectId: "assignment9-realestates",
-  storageBucket: "assignment9-realestates.appspot.com",
-  messagingSenderId: "191146754909",
-  appId: "1:191146754909:web:5c387c0897a11dee955eb4",
-  measurementId: "G-Y0G25ZLKKD"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
--- src/Firebase/hook/usePasswordToggle.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { FaRegEye, FaRegEyeSlash  } from "react-icons/fa";
-
-const usePasswordToggle = () => {
-    const [visible, setVisiblity] = useState(false);
-    const icon = <Fara
-};
-
-export default usePasswordToggle;
